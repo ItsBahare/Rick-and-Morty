9cb628b757230dc0de6eaf233fed68bfb767773f
Bahare <bahare.am78@gmail.com>
2023-11-20 13:20:31 +0330

    create Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div>
-      <h1>hi</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { HeartIcon } from "@heroicons/react/24/outline";
+function Navbar() {
+  return (
+    <div className="navbar">
+      <ul className="navbar_list">
+        <li className="navbar_item">logo</li>
+        <li className="navbar_item">
+          <input
+            type="text"
+            className="input input-search"
+            placeholder="search ..."
+          />
+        </li>
+        <button className="heart">
+          <HeartIcon className="icon" />
+          <span className="badge">4</span>
+        </button>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
